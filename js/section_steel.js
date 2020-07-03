b7b8150755d45c73746ee5b1a61b2586a1f8bb61
CCCTRL <64875199+CCCTRL@users.noreply.github.com>
2020-07-04 06:59:30 +0800

    123

---
+++ js/section_steel.js
@@ -0,0 +1,22 @@
+var i_beam = [
+    {model: "10", weight: 11.3, sfArea: 0.432, crArea: 14.33},
+    {model: "12", weight: 14, sfArea: 0.432, crArea: 14.33},
+    {model: "12.6", weight: 14.2, sfArea: 0.432, crArea: 14.33},
+    {model: "14", weight: 16.9, sfArea: 0.432, crArea: 14.33},
+    {model: "16", weight: 20.5, sfArea: 0.432, crArea: 14.33},
+    {model: "18", weight: 24.1, sfArea: 0.432, crArea: 14.33},
+    {model: "20a", weight: 27.9, sfArea: 0.432, crArea: 14.33},
+    {model: "20b", weight: 31.1, sfArea: 0.432, crArea: 14.33},
+    {model: "22a", weight: 33.1, sfArea: 0.432, crArea: 14.33}
+];
+
+var c_beam = [
+    {model: "10", weight: 11.3, sfArea: 0.432, crArea: 14.33},
+    {model: "10", weight: 11.3, sfArea: 0.432, crArea: 14.33},
+    {model: "10", weight: 11.3, sfArea: 0.432, crArea: 14.33},
+    {model: "10", weight: 11.3, sfArea: 0.432, crArea: 14.33},
+    {model: "10", weight: 11.3, sfArea: 0.432, crArea: 14.33},
+    {model: "10", weight: 11.3, sfArea: 0.432, crArea: 14.33},
+    {model: "10", weight: 11.3, sfArea: 0.432, crArea: 14.33},
+    {model: "10", weight: 11.3, sfArea: 0.432, crArea: 14.33}
+];
